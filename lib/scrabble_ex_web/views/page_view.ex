233c3ed538051924781d901bf76d9bf649b18ed6
zvkemp <zvkemp@gmail.com>
2020-04-15 08:45:13 -0700

    show turn in landing page

--- lib/scrabble_ex_web/views/page_view.ex
+++ lib/scrabble_ex_web/views/page_view.ex
@@ -9,4 +9,14 @@ defmodule ScrabbleExWeb.PageView do
     end)
     |> Enum.join(", ")
   end
+
+  def show_game_turn(_, %{game_over: true}), do: "game over"
+  def show_game_turn(%{assigns: %{player: player}}, %{current_player: current_player}) do
+    if player == current_player do
+      raw "<span class='your-turn'>your turn</span>"
+    else
+      "#{current_player}'s turn"
+    end
+  end
+  def show_game_turn(_, _), do: ""
 end
